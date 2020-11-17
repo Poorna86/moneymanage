17cde7a968846866fad095279815e88ac814c22c
Poorna <poornacboba@gmail.com>
2020-11-17 21:46:19 +0530

    setup env variables for prod version

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,5 @@
 import * as firebase from 'firebase';
-console.log('apiKey: ', process.env.FIREBASE_API_KEY)
-console.log('authDomain: ', process.env.FIREBASE_AUTH_DOMAIN)
-console.log('databaseURL: ', process.env.FIREBASE_DATABASE_URL)
-console.log('projectId: ', process.env.FIREBASE_PROJECT_ID)
-console.log('storageBucket: ', process.env.FIREBASE_STORAGE_BUCKET)
-console.log('messagingSenderId: ', process.env.FIREBASE_MESSAGING_SENDER_ID)
+
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
