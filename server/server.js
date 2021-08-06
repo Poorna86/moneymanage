62d87bb932264740e489c993d6e8d13f0633c8d7
Poorna <poornacboba@gmail.com>
2021-08-06 13:01:02 +0530

    fixing server response

--- server/server.js
+++ server/server.js
@@ -18,22 +18,15 @@ app.get('*', (req, res) => {
 })
 
 app.post('/sendEmail/add', (req, res) => {
-    console.log('expense: ', req.body.loginEmail)
     const add = 'added'
     sendEmailReport(req.body.loginEmail,req.body.name1,req.body.name2,req.body.amount, req.body.description, add)
     res.status(201)
 })
 
 app.post('/sendEmail/edit', (req, res) => {
-    try{
-        const edit = 'Edited'
-        sendEmailReport(req.body.loginEmail, req.body.name1, req.body.name2, req.body.amount, req.body.description, edit)
-        res.status(201).send('success')
-    } catch (err) {
-        console.log(err)
-        res.status(401).send('error')
-    }
-
+    const edit = 'Edited'
+    sendEmailReport(req.body.loginEmail, req.body.name1, req.body.name2, req.body.amount, req.body.description, edit)
+    res.status(201)
 })
 
 app.post('/sendEmail/delete', (req, res) => {
