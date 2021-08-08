3c35b83ea575813d724f062e107a21692c9530fe
Poorna <poornacboba@gmail.com>
2021-08-09 01:44:11 +0530

    test prod for email errors

--- server/router/errorLoggingRouter.js
+++ server/router/errorLoggingRouter.js
@@ -7,7 +7,7 @@ errorLogRouter.post('/sendEmail/edit', async(req, res) => {
     ErrorLogs.init()
     try{
         const edit = 'Edited'
-        const errorLoggs = new ErrorLogs(req.body, {errorMsg: err})
+        const errorLoggs = new ErrorLogs(req.body, {errorMsg: 'errors'})
         await errorLoggs.save()
         sendEmailReport(req.body.loginEmail, req.body.name1, req.body.name2, req.body.amount, req.body.description, edit)
         res.status(201).send()
